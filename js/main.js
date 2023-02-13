7556e8e566f014ec750804b3e7b4f9ad31dc9fe4
luciosantibañez <luciosantibanez99@gmail.com>
2023-02-13 16:19:29 -0300

    pre entrega 1 terminada

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-let nombre = prompt("Hola, cual es tu nombre?")
-alert("Hola " + nombre)
+const btn = document.getElementById("btn");
+const numberInput = document.getElementById("numberInput");
+const multiplicationTable = document.getElementById("multiplicationTable");
 
+btn.addEventListener("click", function() {
+  const number = numberInput.value;
+  let tableHTML = "";
+
+  for (let i = 1; i < 10; i++) {
+    tableHTML += "<tr>";
+    tableHTML += "<td>" + number + "</td>";
+    tableHTML += "<td>x</td>";
+    tableHTML += "<td>" + i + "</td>";
+    tableHTML += "<td>=</td>";
+    tableHTML += "<td>" + number * i + "</td>";
+    tableHTML += "</tr>";
+  }
+
+  multiplicationTable.innerHTML = tableHTML;
+});
